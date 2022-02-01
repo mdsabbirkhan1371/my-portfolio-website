420313a9e681146f7b280ff3f6778a1a389f7b99
Programing-School <mohamedehabam99@hotmail.com>
2022-02-01 15:15:54 +0200

    Initial commit

---
+++ src/data/servicesData.js
@@ -0,0 +1,96 @@
+/* eslint-disable */
+import { BiShoppingBag, BiPencil } from "react-icons/bi";
+import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
+import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
+import {
+  FaInternetExplorer,
+  FaChalkboardTeacher,
+  FaCameraRetro,
+  FaPinterest,
+  FaVideo,
+  FaTabletAlt,
+  FaRegNewspaper,
+  FaSass,
+  FaReact,
+} from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+
+ 
+
+export const servicesData = [
+  // {
+  //     id: 1,
+  //     title: 'Marketing',
+  //     icon: <BiShoppingBag/>
+  // },
+  {
+    id: 2,
+    title: "Content Writing",
+    icon: <BiPencil />,
+  },
+  {
+    id: 3,
+    title: "Email Marketing",
+    icon: <AiOutlineMail />,
+  },
+  // {
+  //     id: 4,
+  //     title: 'Internet Research',
+  //     icon: <FaInternetExplorer />
+  // },
+  // {
+  //     id: 5,
+  //     title: 'Tutoring',
+  //     icon: <FaChalkboardTeacher />
+  // },
+
+  {
+    id: 6,
+    title: "Web Design",
+    icon: <BsCodeSlash />,
+  },
+  // {
+  //     id: 8,
+  //     title: 'Pinterest Virtual Assistant',
+  //     icon: <FaPinterest />
+  // },
+  {
+    id: 9,
+    title: "Video Editing",
+    icon: <FaVideo />,
+  },
+  // {
+  //     id: 10,
+  //     title: 'Data Entry',
+  //     icon: <BsClipboardData />
+  // },
+  // {
+  //     id: 12,
+  //     title: 'Audio Transcription',
+  //     icon: <AiFillAudio />
+  // },
+  {
+    id: 13,
+    title: "Blog Posting",
+    icon: <FaRegNewspaper />,
+  },
+  {
+    id: 14,
+    title: "Sass",
+    icon: <FaSass />,
+  },
+  {
+    id: 15,
+    title: "React",
+    icon: <FaReact />,
+  },
+  {
+    id: 15,
+    title: "TailwindCSS",
+    icon: <SiTailwindcss />,
+  },
+];
+
+// Uncomment your required service.
+// Couldn't find the required services? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+// You can also add on your own 😉.
