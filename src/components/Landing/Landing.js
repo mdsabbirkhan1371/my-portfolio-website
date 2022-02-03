b4fe3cbce002d138349ecbc70b7c70977970e486
Programing-School <mohamedehabam99@hotmail.com>
2022-02-03 14:04:18 +0200

    Code

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -23,52 +23,55 @@ function Landing() {
     const { theme, drawerOpen } = useContext(ThemeContext);
 
     const useStyles = makeStyles((t) => ({
-        resumeBtn: {
-            color: theme.primary,
-            borderRadius: '30px',
-            textTransform: 'inherit',
-            textDecoration: 'none',
-            width: '150px',
-            fontSize: '1rem',
-            fontWeight: '500',
-            height: '50px',
-            fontFamily: 'var(--primaryFont)',
-            border: `3px solid ${theme.primary}`,
-            transition: '100ms ease-out',
-            '&:hover': {
-                backgroundColor: theme.tertiary,
-                color: theme.secondary,
-                border: `3px solid ${theme.tertiary}`,
-            },
-            [t.breakpoints.down('sm')]: {
-                width: '180px',
-            },
+      socialIcon: {
+        color: "#555",
+      },
+      resumeBtn: {
+        color: theme.primary,
+        borderRadius: "30px",
+        textTransform: "inherit",
+        textDecoration: "none",
+        width: "150px",
+        fontSize: "1rem",
+        fontWeight: "500",
+        height: "50px",
+        fontFamily: "var(--primaryFont)",
+        border: `3px solid ${theme.primary}`,
+        transition: "100ms ease-out",
+        "&:hover": {
+          backgroundColor: theme.tertiary,
+          color: theme.secondary,
+          border: `3px solid ${theme.tertiary}`,
         },
-        contactBtn: {
-            backgroundColor: theme.primary,
-            color: theme.secondary,
-            borderRadius: '30px',
-            textTransform: 'inherit',
-            textDecoration: 'none',
-            width: '150px',
-            height: '50px',
-            fontSize: '1rem',
-            fontWeight: '500',
-            fontFamily: 'var(--primaryFont)',
-            border: `3px solid ${theme.primary}`,
-            transition: '100ms ease-out',
-            '&:hover': {
-                backgroundColor: theme.secondary,
-                color: theme.tertiary,
-                border: `3px solid ${theme.tertiary}`,
-            },
-            [t.breakpoints.down('sm')]: {
-                display: 'none',
-            },
+        [t.breakpoints.down("sm")]: {
+          width: "180px",
         },
-        h1tag: {
-            fontSize: '1.5rem'
-        }
+      },
+      contactBtn: {
+        backgroundColor: theme.primary,
+        color: theme.secondary,
+        borderRadius: "30px",
+        textTransform: "inherit",
+        textDecoration: "none",
+        width: "150px",
+        height: "50px",
+        fontSize: "1rem",
+        fontWeight: "500",
+        fontFamily: "var(--primaryFont)",
+        border: `3px solid ${theme.primary}`,
+        transition: "100ms ease-out",
+        "&:hover": {
+          backgroundColor: theme.secondary,
+          color: theme.tertiary,
+          border: `3px solid ${theme.tertiary}`,
+        },
+        [t.breakpoints.down("sm")]: {
+          display: "none",
+        },
+      },
+      h1tag: {
+        fontSize: "1.5rem",
+      },
     }));
 
     const classes = useStyles();
